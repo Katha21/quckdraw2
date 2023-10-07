266f5dcf72ae399cbf8b60c4746853faa5808a9a
Katha21 <112256276+Katha21@users.noreply.github.com>
2023-10-07 23:24:36 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup(){
+    canvas = createCanvas(280,280);
+    canvas.center();
+    background("white");
+}
+
+function draw(){}
+function check_sketch(){}
+
+function updateCanvas(){
+    background("white");
+
+}
